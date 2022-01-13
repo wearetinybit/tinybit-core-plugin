d688578624cbc56281a54936b668c795a3009ad8
Daniel Bachhuber <daniel@bachhuber.co>
2022-01-13 07:09:53 -0800

    Initial commit

---
+++ tinybit-core.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Plugin Name:     TinyBit Core
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     tinybit-core
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         TBC
+ */
+
+/**
+ * Register the class autoloader
+ */
+spl_autoload_register(
+	function( $class ) {
+		$class = ltrim( $class, '\\' );
+		if ( 0 !== stripos( $class, 'TBC\\' ) ) {
+			return;
+		}
+
+		$parts = explode( '\\', $class );
+		array_shift( $parts ); // Don't need "TBC".
+		$last    = array_pop( $parts ); // File should be 'class-[...].php'.
+		$last    = 'class-' . $last . '.php';
+		$parts[] = $last;
+		$file    = dirname( __FILE__ ) . '/inc/' . str_replace( '_', '-', strtolower( implode( '/', $parts ) ) );
+		if ( file_exists( $file ) ) {
+			require $file;
+		}
+
+	}
+);
+
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	WP_CLI::add_command( 'tinybit-core', 'TBC\CLI' );
+}
