83e5ddb597bbf782692d0596bb0e656a1d5840b5
Daniel Bachhuber <daniel@tinybit.com>
2022-02-21 06:42:01 -0800

    Bump working version to 0.2.3

--- tinybit-core.php
+++ tinybit-core.php
@@ -7,7 +7,7 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     tinybit-core
  * Domain Path:     /languages
- * Version:         0.2.2
+ * Version:         0.2.3
  *
  * @package         TBC
  */
