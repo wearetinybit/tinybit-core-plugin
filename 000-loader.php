6ed91bcfeeb57836798e6dc92e72697bbe3315ee
Jeremy Felt <jeremy.felt@gmail.com>
2023-12-18 16:46:43 -0800

    Avoid reserved `$class` as parameter name

--- 000-loader.php
+++ 000-loader.php
@@ -8,25 +8,25 @@
 /**
  * Registers an array of hooks for a class.
  *
- * @param string $class Class name.
- * @param array  $hooks Hooks to register.
+ * @param string $class_name Class name.
+ * @param array  $hooks      Hooks to register.
  */
-function tbc_register_class_hooks( $class, $hooks ) {
-	foreach ( tbc_parse_register_class_hooks( $class, $hooks ) as $parsed_hook ) {
-		list( $type, $hook, $class, $method, $priority, $args ) = $parsed_hook;
+function tbc_register_class_hooks( $class_name, $hooks ) {
+	foreach ( tbc_parse_register_class_hooks( $class_name, $hooks ) as $parsed_hook ) {
+		list( $type, $hook, $class_name, $method, $priority, $args ) = $parsed_hook;
 		$function = 'action' === $type ? 'add_action' : 'add_filter';
-		$function( $hook, [ $class, $method ], $priority, $args );
+		$function( $hook, [ $class_name, $method ], $priority, $args );
 	}
 }
 
 /**
  * Parses an array of hooks into their appropriate definitions.
  *
- * @param string $class Class name.
- * @param array  $hooks Hooks to register.
+ * @param string $class_name Class name.
+ * @param array  $hooks      Hooks to register.
  * @return array
  */
-function tbc_parse_register_class_hooks( $class, $hooks ) {
+function tbc_parse_register_class_hooks( $class_name, $hooks ) {
 	$parsed_hooks = [];
 	foreach ( $hooks as $hook_data ) {
 		$hook     = array_shift( $hook_data );
@@ -52,24 +52,24 @@ function tbc_parse_register_class_hooks( $class, $hooks ) {
 			$parsed_hooks[] = [
 				$type,
 				$hook,
-				$class,
+				$class_name,
 				$method,
 				$priority,
 				$args,
 			];
 		} else {
 			$type = null;
-			if ( method_exists( $class, 'filter_' . $hook ) ) {
+			if ( method_exists( $class_name, 'filter_' . $hook ) ) {
 				$type   = 'filter';
 				$method = 'filter_' . $hook;
-			} elseif ( method_exists( $class, 'action_' . $hook ) ) {
+			} elseif ( method_exists( $class_name, 'action_' . $hook ) ) {
 				$type   = 'action';
 				$method = 'action_' . $hook;
 			}
 			$parsed_hooks[] = [
 				$type,
 				$hook,
-				$class,
+				$class_name,
 				$method,
 				$priority,
 				$args,
@@ -83,13 +83,13 @@ function tbc_parse_register_class_hooks( $class, $hooks ) {
  * Register the class autoloader
  */
 spl_autoload_register(
-	function ( $class ) {
-		$class = ltrim( $class, '\\' );
-		if ( 0 !== stripos( $class, 'TBC\\' ) ) {
+	function ( $class_name ) {
+		$class_name = ltrim( $class_name, '\\' );
+		if ( 0 !== stripos( $class_name, 'TBC\\' ) ) {
 			return;
 		}
 
-		$parts = explode( '\\', $class );
+		$parts = explode( '\\', $class_name );
 		array_shift( $parts ); // Don't need "TBC".
 		$last    = array_pop( $parts ); // File should be 'class-[...].php'.
 		$last    = 'class-' . $last . '.php';
